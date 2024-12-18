c8f2fe1e26792e5834e71d3e9320cf5661d7815c
tobsilog <jewelfashion101@gmail.com>
2024-12-19 00:12:54 +0800

    text color

--- frontend/app/(root)/doctor/dashboard/page.tsx
+++ frontend/app/(root)/doctor/dashboard/page.tsx
@@ -45,15 +45,15 @@ const DoctorDashboard: React.FC = () => {
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
-      <div className=" flex flex-grow w-full  gap-2 mb-6">
+      <div className=" flex flex-grow w-full text-teal-700 gap-2 mb-6">
         <div className="p-6 bg-white rounded-lg w-1/4 shadow-md">
-          <h2 className="text-3xl font-semibold text-gray-600 mb-2">
+          <h2 className="text-3xl font-semibold mb-2">
             Total Patients
           </h2>
-          <div className="text-6xl font-bold text-teal-600 text-center pt-4">{patients}</div>
+          <div className="text-6xl font-bold text-teal-700 text-center pt-4">{patients}</div>
         </div>
         <div className="p-6 bg-white w-3/4 rounded-lg shadow-md">
-          <h2 className="text-3xl font-semibold text-gray-600 mb-4">
+          <h2 className="text-3xl font-semibold mb-4">
             Appointment Info
           </h2>
           <div className="flex flex-grow gap-4 w-full text-center">
@@ -77,8 +77,8 @@ const DoctorDashboard: React.FC = () => {
         </div>
       </div>
 
-      <div className="p-6 bg-white rounded-lg shadow-md">
-        <h2 className="text-3xl font-semibold text-gray-600 mb-4">
+      <div className="p-6 bg-white rounded-lg shadow-md text-black">
+        <h2 className="text-3xl font-semibold mb-4 text-teal-700">
           Appointment Statistics
         </h2>
         <div className="flex justify-center">
