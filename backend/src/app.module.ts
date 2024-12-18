232f62365730e0755b0bc7cd098ce34854fbbd2f
Erwin Antepuesto <66218784+sciyon@users.noreply.github.com>
2024-12-18 22:51:55 +0800

    fixed consistent issue on page load

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AppointmentModule } from './appointment/appointment.module';
+import { ClinicService } from './clinic/clinic.service';
+import { ClinicController } from './clinic/clinic.controller';
+import { ClinicModule } from './clinic/clinic.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, AppointmentModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AuthModule, AppointmentModule, ClinicModule],
+  controllers: [AppController, ClinicController],
+  providers: [AppService, ClinicService],
 })
 export class AppModule {}
